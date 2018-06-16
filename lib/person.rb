4a647e6d74130197a8222d230594fb0cedbbe9c5
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-16 03:18:30 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height
   attr_accessor :weight, :handed, :complexion, :t_shirt_size
-  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_widthend
+  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
