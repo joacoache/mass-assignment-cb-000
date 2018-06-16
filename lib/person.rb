b8cae152b21282acb377adaa340bae74a371d247
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-16 03:15:32 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-  attr_accessor :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  attr_accessor :weight, :handed, :complexion, :t_shirt_size,
   attr_accessor :wrist_size, :glove_size, :pant_length, :pant_widthend
 
   def initialize(attributes)
